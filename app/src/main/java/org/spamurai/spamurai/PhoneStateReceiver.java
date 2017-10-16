2f4afe680e70a817a65d7c1164e9b7209b249b30
Justin <justinwdick@gmail.com>
2017-10-16 14:41:48 -0600

    Retrieving Contact experiment

--- app/src/main/java/org/spamurai/spamurai/PhoneStateReceiver.java
+++ app/src/main/java/org/spamurai/spamurai/PhoneStateReceiver.java
@@ -1,8 +1,11 @@
 package org.spamurai.spamurai;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
@@ -19,8 +22,21 @@ public class PhoneStateReceiver extends BroadcastReceiver {
             if(state.equals(TelephonyManager.EXTRA_STATE_RINGING)){
                 Toast.makeText(context,"Incoming Call State",Toast.LENGTH_SHORT).show();
                 Toast.makeText(context,"Ringing State Number is -"+incomingNumber,Toast.LENGTH_SHORT).show();
+//Experimenting with retrieving contacts
+                ContentResolver resolver = Context.getContentResolver();
+                Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
 
+                while (cursor.moveToNext()){
+                    String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                    String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
 
+                    Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{ id }, null);
+
+                    while (phoneCursor.moveToNext()){
+                        String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    }
+                }
             }
             if ((state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK))){
                 Toast.makeText(context,"Call Received State",Toast.LENGTH_SHORT).show();
@@ -34,5 +50,7 @@ public class PhoneStateReceiver extends BroadcastReceiver {
         }
 
     }
+
+
 }
 
