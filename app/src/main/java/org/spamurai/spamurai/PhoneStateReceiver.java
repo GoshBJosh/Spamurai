027f8838a033fd75585c700f5d522647724f79d1
Justin <justinwdick@gmail.com>
2017-10-16 15:30:35 -0600

    Removing toast

--- app/src/main/java/org/spamurai/spamurai/PhoneStateReceiver.java
+++ app/src/main/java/org/spamurai/spamurai/PhoneStateReceiver.java
@@ -20,19 +20,15 @@ public class PhoneStateReceiver extends BroadcastReceiver {
             String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
 
             if(state.equals(TelephonyManager.EXTRA_STATE_RINGING)){
-                Toast.makeText(context,"Incoming Call State",Toast.LENGTH_SHORT).show();
-                Toast.makeText(context,"Ringing State Number is -"+incomingNumber,Toast.LENGTH_SHORT).show();
 
-
-
-
+                
                 //Experimenting with retrieving contacts
                 ContentResolver resolver = context.getContentResolver();
                 Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
 
                 while (cursor.moveToNext()){
-                    String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-                    String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+//                    String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+//                    String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
 
                     Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
                             ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{ id }, null);
@@ -55,12 +51,7 @@ public class PhoneStateReceiver extends BroadcastReceiver {
 
 
             }
-            if ((state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK))){
-                Toast.makeText(context,"Call Received State",Toast.LENGTH_SHORT).show();
-            }
-            if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)){
-                Toast.makeText(context,"Call Idle State",Toast.LENGTH_SHORT).show();
-            }
+
         }
         catch (Exception e){
             e.printStackTrace();
