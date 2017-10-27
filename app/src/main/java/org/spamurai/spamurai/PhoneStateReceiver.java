8d579a600221f639ed92813b433468bc47ea0271
Justin <justinwdick@gmail.com>
2017-10-27 14:09:51 -0600

    FIxed equation

--- app/src/main/java/org/spamurai/spamurai/PhoneStateReceiver.java
+++ app/src/main/java/org/spamurai/spamurai/PhoneStateReceiver.java
@@ -11,6 +11,8 @@ import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 
 public class PhoneStateReceiver extends BroadcastReceiver {
     @Override
@@ -33,29 +35,29 @@ public class PhoneStateReceiver extends BroadcastReceiver {
                 Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
 
                 while (cursor.moveToNext()){
+                    String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                    String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
 
                     Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
                             ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{ id }, null);
 
-                    String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                    String contactAsString = phoneNumber.replaceAll("[()\\s-]+", "");
-
-                    if(incomingAsString == contactAsString) {
-                        Toast.makeText(context, "Number matches", Toast.LENGTH_SHORT).show();
-                    }
-                    if(incomingAsString != contactAsString) {
-                        Toast.makeText(context, "Number DOES NOT match", Toast.LENGTH_SHORT).show();
-
-                        AudioManager am;
-                        am= (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-                        am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-
-                    }
-
-
-
                     while (phoneCursor.moveToNext()){
+                        String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
+                        String contactAsString = phoneNumber.replaceAll("[()\\s-]+", "");
+                        Log.i("stuff", incomingAsString);
+                        Log.i("Stuff", contactAsString);
+                        if(Objects.equals(incomingAsString, contactAsString)) {
+                            Toast.makeText(context, "Number matches", Toast.LENGTH_SHORT).show();
+                        }
+                        if(!Objects.equals(incomingAsString, contactAsString)) {
+                            Toast.makeText(context, "Number DOES NOT match", Toast.LENGTH_SHORT).show();
+
+                            AudioManager am;
+                            am= (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+                            am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+
+                        }
 
 
                     }
