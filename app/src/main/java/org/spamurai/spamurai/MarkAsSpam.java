ef9021851c2dd06cee9cfb5570b30bc7bb667575
Justin <justinwdick@gmail.com>
2017-11-10 15:09:11 -0700

    Popup Window

--- app/src/main/java/org/spamurai/spamurai/MarkAsSpam.java
+++ app/src/main/java/org/spamurai/spamurai/MarkAsSpam.java
@@ -16,6 +16,26 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MarkAsSpam extends AppCompatActivity {
+    private PopupWindow popWin;
+    private void showPopupWin() {
+        try {
+            LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.popwin, (ViewGroup) findViewById(R.id.popup));
+
+            popWin = new PopupWindow(layout, 290, 360, true);
+            popWin.showAtLocation(layout, Gravity.CENTER, 0, 0);
+            Button exit_popupWin = (Button) layout.findViewById(R.id.exit_popup);
+            exit_popupWin.setOnClickListener(cancel_button);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private View.OnClickListener cancel_button = new View.OnClickListener() {
+        public void onClick(View v) {
+            popWin.dismiss();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,60 +57,36 @@ public class MarkAsSpam extends AppCompatActivity {
 
 
 
-
         // MARK AS SPAM, UNKNOWN, NOT SPAM
-        final RadioGroup markSpamGroup = (RadioGroup) findViewById(R.id.radio_markspamgroup);
-
-        markSpamGroup.setOnClickListener(new View.OnClickListener() {
+        RadioGroup markSpamGroup = (RadioGroup) findViewById(R.id.radio_markspamgroup);
+        markSpamGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
             @Override
-            public void onClick(View v) {
-                // get selected radio button from radioGroup
-                int selectedId = markSpamGroup.getCheckedRadioButtonId();
-                // find the radiobutton by returned id
-                RadioButton thisMarkedOption = (RadioButton) findViewById(selectedId);
-
-
-            }
-            private PopupWindow popWin;
-            private void showPopupWin() {
-                try {
-                    LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                    View layout = inflater.inflate(R.layout.popup, (ViewGroup) findViewById(R.id.popup));
-
-                    popWin = new PopupWindow(layout, 290, 360, true);
-                    popWin.showAtLocation(layout, Gravity.CENTER, 0, 0);
-                    Button exit_popupWin = (Button) layout.findViewById(R.id.exit_popup);
-                    exit_popupWin.setOnClickListener(cancel_button);
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // checkedId is the RadioButton selected
+                if (checkedId == R.id.radio_marknotspam) {
+                    Toast.makeText(getApplicationContext(), "Number DOES NOT match", Toast.LENGTH_SHORT).show();
+                    showPopupWin();
                 }
-                catch (Exception e) {
-                    e.printStackTrace();
+                if (checkedId == R.id.radio_markspam) {
+                    Toast.makeText(getApplicationContext(), "Number DOES match", Toast.LENGTH_SHORT).show();
+
                 }
             }
-
-            private View.OnClickListener cancel_button = new View.OnClickListener() {
-                public void onClick(View v) {
-                    popWin.dismiss();
-                }
-            };
-
-        public void onRadioButtonClicked(View view) {
-            // Is the button now checked?
-            boolean checked = ((RadioButton) view).isChecked();
-            Button exit_popupWin;
-            // Check which radio button was clicked
-            switch(view.getId()) {
-                case R.id.radio_marknotspam:
-                            if (checked)
-
-                            showPopupWin();
-                break;
-                case R.id.radio_markspam:
-                    if (checked)
-                        // Ninjas rule
-                        break;
-            }
-        }
         });
+//
+//
+
+
+//        public void markSpamGroup (View view) {
+//            // Is the button now checked?
+//            boolean checked = ((RadioButton) view).isChecked();
+//            Button exit_popupWin;
+//            // Check which radio button was clicked
+//
+//            }
+//        }
+
 
         final Button checkRegistry = (Button)findViewById(R.id.checkRegistry);
 
